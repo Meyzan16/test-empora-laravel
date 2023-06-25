a1030f041695234a202f121570572cc6c6c0474b
Meyzan16 <Meyzan1605@gmail.com>
2023-06-25 15:10:55 +0700

    create akun seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        //akun seeder
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'username' => 'user',
